fb4a809b962dd721528dcb0531a75293e5ae3576
Olarn U <olarn.u@gmail.com>
2016-05-28 09:44:23 +0700

    Initial Commit

---
+++ CapitalMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CapitalMe
+//
+//  Created by Olarn U. on 5/28/2559 BE.
+//  Copyright © 2559 Olarn U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
