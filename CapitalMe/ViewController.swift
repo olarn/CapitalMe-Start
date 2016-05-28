602fdd91794c0ed8b05b62cb52c54bc5f5ebc311
Olarn U <olarn.u@gmail.com>
2016-05-28 09:47:34 +0700

    Ready to start

--- CapitalMe/ViewController.swift
+++ CapitalMe/ViewController.swift
@@ -2,14 +2,25 @@
 //  ViewController.swift
 //  CapitalMe
 //
-//  Created by Olarn U. on 5/28/2559 BE.
+//  Created by Olarn U. on 5/20/2559 BE.
 //  Copyright © 2559 Olarn U. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    // MARK: - Outlets
 
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var lblCapitalName: UILabel!
+    @IBOutlet var lblCountryCode: UILabel!
+    @IBOutlet var lblCountryName: UILabel!
+    @IBOutlet var lblLatLong: UILabel!
+    @IBOutlet var lblPopulation: UILabel!
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func didTouchOnView(sender: AnyObject) {
+        self.searchBar.resignFirstResponder()
+    }
+    
+    // MARK: - UI Search Bar Delegates Handler
+    
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.showsCancelButton = true
+        return true
+    }
+    
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.showsCancelButton = false
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        self.searchCapitalCityDetail(searchBar.text!)
+    }
+    
+    // MARK: - Search Capital Name
+    
+    private func searchCapitalCityDetail(cityName: String) {
 
-
+    }
+    
 }
 
